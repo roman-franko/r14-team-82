8ec7422522d9d1b7a053d88a386747e5ee5c8e73
boonatik <frankonataliya@gmail.com>
2014-10-19 01:26:23 +0300

    add bootstrap and make navbar

--- app/assets/javascripts/timeline_file.js
+++ app/assets/javascripts/timeline_file.js
@@ -1,7 +1,7 @@
   $(document).ready(function() {
     createStoryJS({
       type: 'timeline',
-      width: '400',
+      width: '100%',
       height: '500',
       source: '/events.json', //get the events.json format from https://github.com/VeriteCo/TimelineJS#file-formats
       embed_id: 'time_line'
