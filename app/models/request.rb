af7c3b6352233940609e0ad4290107d3810576a2
Roman Franko <r.franko@drooms.com>
2014-10-18 23:14:47 +0300

    add validation for request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,5 @@
 class Request < ActiveRecord::Base
   has_many :entities, inverse_of: :request, dependent: :destroy
+
+  validates :text, presence: true
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe Request, :type => :model do
   describe 'factory' do
     it { expect(create(:request)).to be_valid }
   end
+
+  describe 'validations' do
+
+    it 'valid data' do
+      expect(Request.create(text: "request text")).to be_valid
+    end
+
+    describe 'text' do
+
+      context "presence" do
+        let(:request) { Request.create }
+
+        it { expect(request).not_to be_valid }
+
+        it "isn't valid" do
+          expect(request.errors.full_messages).to eq ["Text can't be blank"]
+        end
+      end
+
+    end
+  end
 end
