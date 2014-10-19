6de97445cb733291b7bad92d25813b018d407cff
virtax <partsirniy@mail.ru>
2014-10-19 20:59:49 +0300

    model

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,3 +1,16 @@
+require 'wiki_parser'
+
 class Entity < ActiveRecord::Base
+
+  def load(keyword)
+    w =  WikiParser.new
+    w.load(keyword)
+    self.start_date = w.start_date
+    self.end_date = w.end_date
+    self.headline = w.title
+    #self.text = w.html
+    self.media = w.url
+    save
+  end
 
 end
